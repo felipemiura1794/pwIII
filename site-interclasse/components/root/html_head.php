1f05debc7ca6a794f7a26763b0e7ce71a7e19add
yDewolf <andrersng@gmail.com>
2026-04-14 21:18:39 -0300

    HTML - Base Navbar

--- site-interclasse/components/root/html_head.php
+++ site-interclasse/components/root/html_head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Título da Página</title>
-    <link rel="stylesheet" href="public/.css">
+    <link rel="stylesheet" href="public/style/general.css">
 </head>
 <body>
 
