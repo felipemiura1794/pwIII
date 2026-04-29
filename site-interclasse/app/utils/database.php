5c6158f58db8973557a73b7a8f842a9d4436a1ac
yDewolf <andrersng@gmail.com>
2026-04-29 10:46:19 -0300

    Create Database class

---
+++ site-interclasse/app/utils/database.php
@@ -0,0 +1,38 @@
+<?php
+
+class Database {
+    private static Database | null $instance = null;
+    
+    private string $host = '127.0.0.1';
+    private string $db   = 'test_db';
+    private string $username = 'root';
+    private string $password = '';
+    private string $charset = 'utf8mb4';
+    private PDO $conn;
+
+    private function __construct() {
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        try {
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            echo "Database Connected";
+        
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+
+// Pra usar:
+// $database = Database::getInstance()->getConnection();
